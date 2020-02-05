2bec9a47bac103116b23286fd3bac68ca04dd2ab
ahmadysalah <a.ahmad.salah@gmail.com>
2020-02-05 09:11:35 +0200

    hide api keys from github and repair the url #19

--- js/dom.js
+++ js/dom.js
@@ -1,21 +1,19 @@
-const searchBtn = document.querySelector('#searchButton');
 const wrapper = document.querySelector('.wrapper');
 
-function changebackground(search) {
-  apiFunc(UrlUnsplash + search, responseText => {
+const changebackground = search => {
+  apiFunc(UrlUnsplash + ApiunsplashKey + '&query=' + search, responseText => {
     let BgArr = [];
     for (i in responseText.results) {
       // put all results in array);
       BgArr = [...BgArr, responseText.results[i].urls.regular];
     }
-    console.log(BgArr);
+    //  console.log(BgArr);
     wrapper.style.backgroundImage = `url('${BgArr[0]}')`;
-    //BgArr = [];
   });
-}
+};
 
+const searchBtn = document.querySelector('#searchButton');
 searchBtn.addEventListener('click', () => {
   const searchInput = document.querySelector('#searchInput');
   changebackground(searchInput.value);
-  // console.log(searchInput.value);
 });
--- js/xhr.js
+++ js/xhr.js
@@ -1,6 +1,5 @@
 const UrlWeather = '';
-const UrlUnsplash =
-  'https://api.unsplash.com/search/photos?client_id=1424074b20f17701ec8c0601fd15ca686c70e2cb0e645f8137533d8063e664bc&query=';
+const UrlUnsplash = 'https://api.unsplash.com/search/photos?client_id=';
 const apiFunc = (url, callback) => {
   const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = () => {
